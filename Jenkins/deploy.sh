c8e9bd373e0fc2decd34456e1ed4aee2cd2938f8
Chance <ahs0432@naver.com>
2025-05-10 21:58:02 +0900

    ✅ Upload Jenkins deploy script

---
+++ Jenkins/deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+JAR_PATH=/home/jar
+JAR_NAME=cquec-0.0.1-SNAPSHOT.jar
+
+#cd $JAR_PATH
+JAVA_PID=$(pgrep -f ${JAR_NAME})
+
+if [ -z $JAVA_PID ]; then
+        echo "프로세스가 실행되고 있지 않습니다."
+else
+        echo "대상 프로세스 $JAVA_PID를 Kill 처리하였습니다."
+        kill -9 $JAVA_PID
+        sleep 5
+fi
+
+nohup java -jar $JAR_PATH/$JAR_NAME > nohup.out 2>&1 &
